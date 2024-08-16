c29a6e2c73e968e27cdc017cd34ad4129aa4e564
aya.gallarte <aya.gallarte@gmail.com>
2024-08-16 16:30:56 +0800

    Added react icons

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,8 +2,12 @@ import React, { useContext } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 import UserContext from '../context/UserContext';
-
 import logo from '../images/logo.png'; 
+import { FaHome, FaTshirt, FaShoppingCart, FaListAlt } from "react-icons/fa";
+import { IoLogOut, IoLogIn } from "react-icons/io5";
+import { MdBorderColor } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+
 import '../style.css';
 
 export default function AppNavbar() {
@@ -24,26 +28,26 @@ export default function AppNavbar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" className="navToggle" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/products">Products</Nav.Link>
+                        <Nav.Link as={NavLink} to="/"><FaHome /> Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/products"><FaTshirt /> Products</Nav.Link>
                     </Nav>
 
                     <Nav className="ms-auto">
                         {user && user.id !== null && user.id !== undefined ? (
                             user.isAdmin ? (       
-                                <Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
+                                <Nav.Link as={NavLink} to="/logout"><IoLogOut /> Logout</Nav.Link>
                             ) : (
                                 <>
-                                    <Nav.Link as={NavLink} to="/cart">Cart</Nav.Link>
-                                    <Nav.Link as={NavLink} to="/order">Order</Nav.Link>
-                                    <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
-                                    <Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/order"><FaListAlt /> Order</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/profile"><CgProfile /> Profile</Nav.Link>
+                                    <Nav.Link as={NavLink} to="/logout"><IoLogOut /> Logout</Nav.Link>
                                 </>
                             )
                         ) : (
                             <>
-                                <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
-                                <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
+                                <Nav.Link as={NavLink} to="/login"><IoLogIn /> Login</Nav.Link>
+                                <Nav.Link as={NavLink} to="/register"><MdBorderColor /> Register</Nav.Link>
                             </>
                         )}
                     </Nav>
