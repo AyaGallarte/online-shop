bc890c7250a83e9196bd8aae266635c89494b059
aya.gallarte <aya.gallarte@gmail.com>
2024-08-19 08:18:47 +0800

    Fetched products upon loading of home page

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
                   </Routes>
               </Container>
             </Router>
-          <ProductsProvider>
+          </ProductsProvider>
         </ProgressProvider>
       </UserProvider>
   );
